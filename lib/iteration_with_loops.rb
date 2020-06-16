58524b30193978f3eed23cc30a2022ff35b0daab
Sam Evans <evansst@icloud.com>
2020-06-16 02:54:36 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
     if src[row_index][element_index].is_a? String
-    output = output + src[row_index][element_index]
+    output = output + ' ${src[row_index][element_index]}'
     end
     element_index += 1
   end
